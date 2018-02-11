5f05c70ac09e0661b0c11afa8301244441da48a4
Garrett Moore <gmoore10@student.fullsail.edu>
2018-02-11 18:36:55 -0500

    NodeJS Server Set up for Week 2

---
+++ week2/hello.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+var server = http.createServer(function(request, response) {
+    response.writeHead(200,{"Content-Typee": "text/plain"});
+
+    response.end("HELLO");
+});
+
+server.listen(80);
+
+console.log("Everything is fine and server is running");
