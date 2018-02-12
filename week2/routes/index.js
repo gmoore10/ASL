ed120092f8fd5c9f16bd63b18a37971a5a920c1d
Garrett Moore <gmoore10@student.fullsail.edu>
2018-02-11 19:33:45 -0500

    Navigation Set up

--- week2/routes/index.js
+++ week2/routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Home',
+    navitems: [
+      {link: '/', content: 'Home'},
+      {link: '/users', content: 'Users'}
+    ]
+  });
+});
+
+/* GET users page. */
+router.get('/users', function(req, res, next) {
+  res.render('users', { 
+    title: 'Users',
+    navitems: [
+      {link: '/', content: 'Home'},
+      {link: '/users', content: 'users'}
+    ]
+  });
 });
 
 module.exports = router;
