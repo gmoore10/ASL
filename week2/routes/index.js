b1c4c15cc7d9e599ab714d33745729a3c45bf6a7
Garrett Moore <gmoore10@student.fullsail.edu>
2018-02-11 20:08:43 -0500

    Form created

--- week2/routes/index.js
+++ week2/routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var navitems = [
+  {link: '/', content: 'Home'},
+  {link: '/users', content: 'Users'},
+  {link: '/register', content: 'Register'}
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Home',
-    navitems: [
-      {link: '/', content: 'Home'},
-      {link: '/users', content: 'Users'}
-    ]
+    navitems: navitems
   });
 });
 
@@ -16,10 +19,15 @@ router.get('/', function(req, res, next) {
 router.get('/users', function(req, res, next) {
   res.render('users', { 
     title: 'Users',
-    navitems: [
-      {link: '/', content: 'Home'},
-      {link: '/users', content: 'users'}
-    ]
+    navitems: navitems
+  });
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { 
+    title: 'Register',
+    navitems: navitems
   });
 });
 
